67cdd8864dcd8e2557803eb4622db6bc90222e97
emalo <elie.jean.malo@gmail.com>
2023-06-28 13:40:41 +0000

    senmoney2.0

--- senmoney[Elie]/main.c
+++ senmoney[Elie]/main.c
@@ -1,73 +1,27 @@
 #include <stdio.h>
 #include "fonctions.h"
 
-void returnMenu()
-{
-    char choix;
 
-    do
-    {
-        printf("Voulez-vous retourner au menu principal? (y/n): ");
-        scanf(" %c", &choix);
-
-        if (choix == 'y')
-        {
-            int numero;
-            printf("Veuillez Saisir votre numero de telephone: ");
-            scanf("%d",&numero);
-            for(int i=0 ;i <5;i++)
-            {
-                if (tabnumeros[i]==numero)
-                {
-                    int indice=trouve(tabnumeros,numero);
-                    int saisi_du_menu= Menu();
-                    if (saisi_du_menu==1)
-                    {
-                        printf("%d\n", affichersoldes(indice));
-                    }else if (saisi_du_menu==2)
-                    {
-                        transfert_d_argent(tabsoldes, tabnumeros, tabcodes, indice);
-                    }
-                    break;
-                }else
-                {
-                    printf("Numero incorrect\n");
-                }
-                
-            }
-        }
-
-    } while (choix == 'y');
-}
 void main()
 {
     int numero;
-    printf("Veuillez Saisir votre numero de telephone: ");
+    printf("Veuillez entrer votre numero: ");
     scanf("%d",&numero);
-    for (int k = 0; k < 5; k++)
+    while (numero!=tabnumeros[0] && numero!=tabnumeros[1] && numero!=tabnumeros[2] && numero!=tabnumeros[3] && numero!=tabnumeros[4] )
     {
-        if (tabnumeros[k]==numero)
-        {
-            int indice=trouve(tabnumeros,numero);
-            int saisi_du_menu= Menu();
-            if (saisi_du_menu == 1)
-                {
-                    printf("%d\n", affichersoldes(indice));
-                    returnMenu();
-                }
-            else if (saisi_du_menu == 2)
-                {
-                    transfert_d_argent(tabsoldes, tabnumeros, tabcodes, indice);
-                    returnMenu();
-                }
-            break;
-        
-        }
-        else
-        {
-            printf("Numero incorrect\n");
-        }
-        
-    }  
-        
+        printf("Numero non present dans la base de donne veuillez entrer un autre numero: ");
+        scanf("%d",&numero);
+    }
+    int indice=trouve(tabnumeros,numero);
+    int saisi_du_menu=Menu();
+    if (saisi_du_menu==1)
+    {
+        printf("%d\n", affichersoldes(indice));
+        returnMenu();
+    }
+    if (saisi_du_menu==2)
+    {
+        transfert_d_argent(tabsoldes, tabnumeros, tabcodes, indice);
+        returnMenu();
+    }
 }
