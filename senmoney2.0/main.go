5d8c8a58a4eaf4ce7000e29f4aa2bc952c373457
emalo <elie.jean.malo@gmail.com>
2023-07-01 17:01:06 +0000

    Senmoney

--- senmoney2.0/main.go
+++ senmoney2.0/main.go
@@ -12,11 +12,63 @@ func Trouve(tab []int, val int) int {
 	}
 	return indice
 }
+func affichersoldes(indice int) int {
+	tabsoldes := [5]int{10000, 150000, 12500, 45000, 32500}
+	return tabsoldes[indice]
+}
 
-func main() {
+func Menu() int {
+	var choix int
+	fmt.Println("### Menu Sen Money ###")
+	fmt.Println("Veuillez saisir le numero du service a utiliser")
+	fmt.Println("1.Afficher votre solde")
+	fmt.Println("2.Transfert d'argent")
+	fmt.Println("3.Options")
+	fmt.Print("Votre choix: ")
+	fmt.Scanln(&choix)
+	return choix
+
+}
+func TransfertArgent(indice int) {
+	var numeroDestinataire int
+	var montant int
+	var motDePasse int
 	tabnumeros := [5]int{779000000, 778000000, 774000000, 775000000, 771000000}
+	tabsoldes := [5]int{10000, 150000, 12500, 45000, 32500}
+	tabcode := [5]int{1111, 1111, 1111, 1111, 1111}
+	indice2 := -1
 
+	for indice2 == -1 {
+		fmt.Print("Veuillezsaisir le numero du destinataire")
+		fmt.Scanln(&numeroDestinataire)
+
+		indice2 = Trouve(tabnumeros[:], numeroDestinataire)
+
+		if indice2 == -1 {
+			fmt.Println("Numéro non trouvé. Veuillez réessayer.")
+		}
+	}
+	fmt.Print("Veuillez saisir le montant a transferer ")
+	fmt.Scanln(&montant)
+	if montant > tabsoldes[indice] {
+		fmt.Println("Soldes Insuffisants")
+	} else if montant <= tabsoldes[indice] {
+		fmt.Print("Veuillez entrer votre mot de passe")
+		fmt.Scanln(&motDePasse)
+		if motDePasse != tabcode[indice] {
+			fmt.Println("Mot de passe incorrect")
+		} else if motDePasse == tabcode[indice] {
+			fmt.Println("Transfert Reussi!")
+			tabsoldes[indice2] += montant
+			tabsoldes[indice] -= montant
+		}
+	}
+
+}
+func returnMenu() {
+	tabnumeros := [5]int{779000000, 778000000, 774000000, 775000000, 771000000}
 	var numero int
+	var choix string
 	indice := -1
 
 	for indice == -1 {
@@ -29,6 +81,53 @@ func main() {
 			fmt.Println("Numéro non trouvé. Veuillez réessayer.")
 		}
 	}
+	ChoixMenu := Menu()
+	if ChoixMenu == 1 {
+		fmt.Println("Votre solde actuel est: ", affichersoldes(indice))
+	} else if ChoixMenu == 2 {
+		TransfertArgent(indice)
+	}
 
-	fmt.Println("Numéro trouvé à l'indice :", indice)
+	fmt.Print("Voulez vous retournez au menu principale: ")
+	fmt.Scanln(&choix)
+
+	if choix == "y" {
+		returnMenu()
+	} else {
+		fmt.Println("Au-Revoir")
+	}
+	fmt.Println("-----------------------------")
+}
+func main() {
+	tabnumeros := [5]int{779000000, 778000000, 774000000, 775000000, 771000000}
+	var numero int
+	var choix string
+	indice := -1
+
+	for indice == -1 {
+		fmt.Print("Entrer votre numéro de téléphone : ")
+		fmt.Scanln(&numero)
+
+		indice = Trouve(tabnumeros[:], numero)
+
+		if indice == -1 {
+			fmt.Println("Numéro non trouvé. Veuillez réessayer.")
+		}
+	}
+	ChoixMenu := Menu()
+	if ChoixMenu == 1 {
+		fmt.Println("Votre solde actuel est: ", affichersoldes(indice))
+	} else if ChoixMenu == 2 {
+		TransfertArgent(indice)
+	}
+
+	fmt.Print("Voulez vous retournez au menu principale: ")
+	fmt.Scanln(&choix)
+
+	if choix == "y" {
+		returnMenu()
+	} else {
+		fmt.Println("Au-Revoir")
+	}
+	fmt.Println("---------------------------------")
 }
--- senmoney[Elie]/main.c
+++ senmoney[Elie]/main.c
@@ -5,23 +5,29 @@
 void main()
 {
     int numero;
-    printf("Veuillez entrer votre numero: ");
-    scanf("%d",&numero);
-    while (numero!=tabnumeros[0] && numero!=tabnumeros[1] && numero!=tabnumeros[2] && numero!=tabnumeros[3] && numero!=tabnumeros[4] )
-    {
-        printf("Numero non present dans la base de donne veuillez entrer un autre numero: ");
-        scanf("%d",&numero);
-    }
-    int indice=trouve(tabnumeros,numero);
-    int saisi_du_menu=Menu();
-    if (saisi_du_menu==1)
-    {
-        printf("%d\n", affichersoldes(indice));
-        returnMenu();
-    }
-    if (saisi_du_menu==2)
-    {
-        transfert_d_argent(tabsoldes, tabnumeros, tabcodes, indice);
-        returnMenu();
-    }
+    char choix;
+    do
+        {
+        do
+        {
+            printf("Veuillez saisir votre numero: ");
+            scanf("%d",&numero);
+        } while (numero != tabnumeros[0]  && numero != tabnumeros[1]  && numero != tabnumeros[2]  && numero != tabnumeros[3]  && numero != tabnumeros[4]);
+        int indice=trouve(tabnumeros,numero);
+        int saisiChoixMenu=Menu();
+        if (saisiChoixMenu==1)
+        {
+            printf("Votre solde est de : %d\n",affichersoldes(indice));
+        }else if (saisiChoixMenu==2)
+        {
+            transfert_d_argent(tabsoldes,tabnumeros,tabcodes,indice);
+        }
+        
+        
+        printf("Voulez vous retournez au menu principale: (y/n) \n");
+        scanf("%s",&choix);
+
+    } while (choix== 'y');
+    
+
 }
